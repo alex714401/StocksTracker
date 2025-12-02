b3e0ea7100a4e7e800801d5b5314ccd5b92919cf
Alex Angelov <Aleksandar.angelov@news.co.uk>
2025-12-02 20:14:24 +0200

    Initial Commit

---
+++ StocksTracker/StocksTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksTrackerApp.swift
+//  StocksTracker
+//
+//  Created by Aleksandar Angelov on 2.12.25.
+//
+
+import SwiftUI
+
+@main
+struct StocksTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StocksTrackerTests/StocksTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StocksTrackerTests.swift
+//  StocksTrackerTests
+//
+//  Created by Aleksandar Angelov on 2.12.25.
+//
+
+import Testing
+@testable import StocksTracker
+
+struct StocksTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
